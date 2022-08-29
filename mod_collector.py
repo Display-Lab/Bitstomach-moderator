a71d63b7b9f8bcb5d5667e110918de38e8314a9a
Ayshwarya Jagadeesan <ayshjag@msla0669.attlocal.net>
2022-08-29 01:49:50 -0400

    read the neg,pos,gaps, trends,social and goal comparator measures designated by the bit stomach- next calculate corresponding trend slopes and gap sizes

--- mod_collector.py
+++ mod_collector.py
@@ -14,7 +14,7 @@ from rdflib.serializer import Serializer
 from rdfpandas.graph import to_dataframe
 from SPARQLWrapper import XML, SPARQLWrapper
 
-from load import read ,read_gaps_trends,read_comparison_values
+from load import read ,read_goal_comparator,read_comparison_values,read_social_comparator,neg_perf_trend,read_gap_con,pos_perf_trend,pos_gap,neg_gap
 #, read_perf_data ,read_gaps
 #,write
 #from .calc_gaps_slopes import calc_gap_sizes ,calc_slope_trends
@@ -27,7 +27,15 @@ warnings.filterwarnings("ignore")
 start_time = time.time()
 graph_read = read(sys.argv[1])
 read_comparison_values = read_comparison_values(graph_read)
-read_gaps_trends = read_gaps_trends(graph_read)
-read_gaps_trends_df = to_dataframe(read_gaps_trends)
-read_gaps_trends_df.to_csv("measures1.csv")
+read_goal_comparator = read_goal_comparator(graph_read)
+read_social_comparator = read_social_comparator(graph_read)
+neg_perf_trend =neg_perf_trend(graph_read)
+pos_perf_trend =pos_perf_trend(graph_read)
+read_gap_con =read_gap_con(graph_read)
+pos_gap =pos_gap(graph_read)
+pos_gap_df = to_dataframe(pos_gap)
+pos_gap_df.to_csv("measures1.csv")
+neg_gap =neg_gap(graph_read)
+neg_gap_df = to_dataframe(neg_gap)
+neg_gap_df.to_csv("measures2.csv")
 
